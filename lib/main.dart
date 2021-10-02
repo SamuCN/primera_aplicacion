55b6b58df6fa275c91f8fda3f5897a94ff3d7503
Samuel Campoy Nieves <scamnie214@g.enducaand.es>
2021-10-02 16:47:32 +0200

    ContadorPage funciona

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:primera_aplicacion/src/app.dart';
 
 void main(){
-  // ignore: unnecessary_new
   runApp(new MyApp());
 
 }
--- lib/src/pages/contador_page.dart
+++ lib/src/pages/contador_page.dart
@@ -1,34 +1,28 @@
-
-
 import 'package:flutter/material.dart';
 
-class ContadorPage extends StatefulWidget{
+class ContadorPage extends StatefulWidget {
 
   @override
-  createState() {
-    
-    return _ContadorPageState();
-  }
-
+  createState() => _ContadorPageState();
 }
 
-class _ContadorPageState extends State<ContadorPage>{
+class _ContadorPageState extends State<ContadorPage> {
+
+  final _estiloTexto = new TextStyle (fontSize: 25);
+  int _contador = 0;
 
   @override
   Widget build(BuildContext context) {
-    final _estiloTexto = new TextStyle (fontSize: 25);
-    int _contador = 0;
-    
-    return Scaffold(
+   return Scaffold(
       appBar: AppBar(
-        title: Text("Titulo"),
+        title: Text("StateFul"),
         centerTitle: true,
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text("Numeros de clickss: ", style:  _estiloTexto),
+            Text("Numeros de clicks: ", style:  _estiloTexto),
             Text("$_contador", style: _estiloTexto),
           ]
         ),
@@ -37,15 +31,12 @@ class _ContadorPageState extends State<ContadorPage>{
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
         onPressed: () {
-
+          _contador++;
           setState(() {
-            _contador++;
+            
           });
-
         },
         ),
     );
-
   }
-
 }
