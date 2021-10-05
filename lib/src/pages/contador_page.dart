5d4cb34ad0229f4977f149d1e8dcce0ace72fbc6
Samuel Campoy Nieves <scamnie214@g.enducaand.es>
2021-10-05 14:35:06 +0200

    Añadiendo botones

--- lib/src/pages/contador_page.dart
+++ lib/src/pages/contador_page.dart
@@ -28,15 +28,39 @@ class _ContadorPageState extends State<ContadorPage> {
         ),
       ),
       //floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () {
-          _contador++;
-          setState(() {
-            
-          });
-        },
-        ),
-    );
+      floatingActionButton: _crearBotones(),
+        );
+  }
+  Widget _crearBotones(){
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: <Widget>[
+        SizedBox(width: 30),
+        FloatingActionButton(child: Icon(Icons.exposure_zero), onPressed: _reset),
+        Expanded(child: SizedBox (width: 5.0)),
+        FloatingActionButton(child: Icon(Icons.remove), onPressed: _restar),
+        SizedBox(width: 5.0),
+        FloatingActionButton(child: Icon(Icons.add), onPressed: _sumar),
+      ],
+    ); 
+    
+  }
+
+  void _sumar(){
+    setState(() {
+      _contador++;
+    });
+  }
+
+  void _restar(){
+    setState(() {
+      _contador--;
+    });
+  }
+
+  void _reset(){
+    setState(() {
+      _contador=0;
+    });
   }
 }
